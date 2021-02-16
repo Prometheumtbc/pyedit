0ef6ee13773942d532f94959296b67306243938f
Prometheumtbc <79119688+Prometheumtbc@users.noreply.github.com>
2021-02-17 09:30:56 +1300

    init

---
+++ src/prim.py
@@ -0,0 +1,58 @@
+import sys
+
+try:
+    # for Python2
+    from Tkinter import *
+    import tkFileDialog
+except ImportError:
+    # for Python3
+    from tkinter import *
+    import tkinter.tkFileDialog
+
+root = Tk("PyEdit")
+text = Text(root) 
+text.grid() 
+
+def saveAs():
+
+    global text
+
+    t = text.get("1.0", "end-1c")
+
+    savelocation = tkFileDialog.asksaveasfilename()
+
+    file1 = open(savelocation, "w+")
+
+    file1.write(t)
+
+    file1.close()
+
+button=Button(root, text="Save", command=saveAs) 
+button.grid()
+
+def fontHelvetica():
+
+    global text
+
+    text.config(font="Helvetica")
+
+def fontCourier():
+
+    global text
+
+    text.config(font="Courier")
+
+
+font = Menubutton(root, text="Font") 
+font.grid() 
+font.menu = Menu(font, tearoff=0) 
+font["menu"] = font.menu
+Helvetica = IntVar() 
+arial = IntVar() 
+times = IntVar() 
+Courier = IntVar()
+font.menu.add_checkbutton(label="Courier", variable=Courier, 
+command=fontCourier)
+font.menu.add_checkbutton(label="Helvetica", variable=Helvetica,
+command=fontHelvetica) 
+root.mainloop()
